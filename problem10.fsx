f914a077886ef4d294825f727fa927c27b97baa9
Dawid Karp <dadulerk@gmail.com>
2022-08-21 00:47:57 +0200

    add problem 10 (solved)

---
+++ problem10.fsx
@@ -0,0 +1,20 @@
+(*
+The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+
+Find the sum of all the primes below two million.
+*)
+
+let isPrime number =
+    let sq = number |> float |> sqrt |> int
+
+    seq { 2..sq }
+    |> Seq.exists (fun i -> number % i = 0)
+    |> not
+
+let primes =
+    (+) 2 // start from 2
+    |> Seq.initInfinite
+    |> Seq.filter isPrime
+    |> Seq.map int64
+
+primes |> Seq.takeWhile ((>) 2000000L) |> Seq.sum
